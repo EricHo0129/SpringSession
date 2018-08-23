8d9c155866c24377051d7228c152e7cb8751c6b3
EricHo0129 <yung.ho@104.com.tw>
2018-08-23 15:22:54 +0800

    加上session scope bean

--- src/main/java/com/eric/springboot/controller/IndexController.java
+++ src/main/java/com/eric/springboot/controller/IndexController.java
@@ -1,21 +1,37 @@
 package com.eric.springboot.controller;
 
 import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.eric.springboot.model.UserInfo;
+
 @Controller
 public class IndexController {
 
+	@Autowired
+	private UserInfo userInfo;
+	
 	@GetMapping("/")
 	public String index(HttpSession session, @RequestParam("name") String name, Model model) {
 		if (!StringUtils.isEmpty(name)) {
 			session.setAttribute("username", name);
 			model.addAttribute("username", name);
 		}
+		return "index";
+	}
+	
+	@GetMapping("/autofill")
+	public String autofill(Model model) {
+		userInfo.setPid(123456L);
+		userInfo.setName("Eric123");
+		model.addAttribute("username", userInfo.getName());
+		
 		return "index";
 	}
 	
---
+++ src/main/java/com/eric/springboot/model/UserInfo.java
@@ -0,0 +1,36 @@
+package com.eric.springboot.model;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
+public class UserInfo implements Serializable {
+	
+	private static final long serialVersionUID = -2409231630393231158L;
+
+	private Long pid;
+	
+	private String name;
+
+	public Long getPid() {
+		return pid;
+	}
+
+	public void setPid(Long pid) {
+		this.pid = pid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
